01d5286fa0e86c2cd4e5ca1faf3a0f0d21f550e4
Zach Tellman <ztellman@gmail.com>
2014-04-20 21:00:14 -0700

    add codox

--- project.clj
+++ project.clj
@@ -6,7 +6,10 @@
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
                                   [collection-check "0.1.2"]
                                   [criterium "0.4.3"]
-                                  [primitive-math "0.1.3"]]}}
+                                  [primitive-math "0.1.3"]
+                                  [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}}
+  :plugins [[codox "0.6.6"]]
+  :codox {:writer codox-md.writer/write-docs}
   :test-selectors {:default (complement :benchmark)
                    :benchmark :benchmark}
   :jvm-opts ["-server"])
